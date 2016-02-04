34ea00d9480c63e1a5522c72f80711e17e28ea2d
Anna <annsta@gmail.com>
2016-02-04 02:39:44 +0200

    Simple Conditional Statements

---
+++ 03. Simple Conditional Statements/Excellent or Not/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excellent_or_Not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.5)
+                Console.WriteLine("Excellent!");
+            else
+                Console.WriteLine("Not excellent.");
+        }
+    }
+}
