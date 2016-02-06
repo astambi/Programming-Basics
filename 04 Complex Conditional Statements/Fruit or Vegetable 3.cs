2e0fe2005d0476cfbde3d30a3b9b9f5e0d970ac8
Anna <annsta@gmail.com>
2016-02-06 20:31:52 +0200

    Complex Conditional Statements

---
+++ 04 Complex Conditional Statements/Fruit or Vegetable 3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit_or_Vegetable_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine().ToLower();
+            string productType = "unknown";
+
+            if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon" || product == "grapes")
+                productType = "fruit";
+            else if (product == "tomato" || product == "cucumber" || product == "pepper" || product == "carrot")
+                productType = "vegetable";
+
+            Console.WriteLine(productType);
+        }
+    }
+}
