95da3501b49a8b6cf772c5ea3877f471d2597b8a
Anna <annsta@gmail.com>
2016-02-10 01:33:25 +0200

    Exams

---
+++ 33 Programming Basics Archive/Programming Basics Exam 2016-01-17/01 Cake Tycoon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Cake_Tycoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakesPlanned = int.Parse(Console.ReadLine());       //n
+            double flourPerCake = double.Parse(Console.ReadLine()); //c kg
+            int flourAvailable = int.Parse(Console.ReadLine());     //f, kg, cost of flour = 0
+            int numTruffles = int.Parse(Console.ReadLine());        // t
+            int priceTruffle = int.Parse(Console.ReadLine());       //p
+
+            double cakesProduced = Math.Min(cakesPlanned, Math.Truncate(flourAvailable / flourPerCake)); // max cakesProduced <= cakesPlanned
+            double priceCake = numTruffles / cakesProduced * priceTruffle * 1.25;   // NB division first!!! cost trufles, 25% profit
+            double flourShortage = cakesPlanned * flourPerCake - flourAvailable;    //kg
+            var shortage = cakesProduced < cakesPlanned;
+
+            if (shortage)
+                Console.WriteLine("Can make only {0} cakes, need {1:0.00} kg more flour", cakesProduced, flourShortage);
+            else
+                Console.WriteLine("All products available, price of a cake: {0:0.00}", priceCake);
+        }
+    }
+}
---
+++ 33 Programming Basics Archive/Programming Basics Exam 2016-01-17/04 Master Herbalist.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Master_Herbalist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dailyExpenses = int.Parse(Console.ReadLine());
+
+            string path = "";
+            int days = 0;
+            int hours = 0;
+            int herbPrice = 0;
+            int numHerbs = 0;
+            double totalMoneyMade = 0;
+            string dailyInput = "";
+            string endOfInput = "Season Over";
+
+            while ((dailyInput = Console.ReadLine()) != endOfInput)
+            {
+                string[] inputDetails = dailyInput.Split(' ');  //splits input into array {hours, path, price}
+                hours = int.Parse(inputDetails[0]);             //parses input 
+                path = inputDetails[1];
+                herbPrice = int.Parse(inputDetails[2]);
+                numHerbs = 0;
+                while (path.Length < hours)                     //loops path if path lenght < hours spends in the mountains
+                    path += inputDetails[1];
+                char[] pathLetters = path.ToCharArray();        //converts path from string to char array
+                for (int i = 0; i < hours; i++)
+                {
+                    if (pathLetters[i] == 'H')                  //counts number of H chars in the array (herbs/day)
+                        numHerbs += 1;           
+                }
+                totalMoneyMade += herbPrice * numHerbs;
+                ++days;
+            }
+            double averageEarnings = totalMoneyMade / days;
+
+            if (averageEarnings >= dailyExpenses)
+                Console.WriteLine("Times are good. Extra money per day: {0:0.00}.", averageEarnings - dailyExpenses);
+            else
+                Console.WriteLine("We are in the red. Money needed: {0:0}.", dailyExpenses * days - totalMoneyMade);
+        }
+    }
+}
