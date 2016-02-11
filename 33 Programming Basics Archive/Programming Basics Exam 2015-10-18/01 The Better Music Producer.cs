d0a36adf6069633fb146d0717c8a10e173219405
Anna <annsta@gmail.com>
2016-02-11 22:10:15 +0200

    Exams

---
+++ 33 Programming Basics Archive/Programming Basics Exam 2015-10-18/01 The Better Music Producer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_The_Better_Music_Producer
+{
+    class _01_The_Better_Music_Producer
+    {
+        static void Main(string[] args)
+        {
+            int[] albums = { 0, 0, 0 };                     // {EUR, NAmerica, SAmerica}
+            decimal[] albumPrice = { 0m, 0m, 0m };          // {EUR, NAmerica, SAmerica}
+            decimal[] fx = { 1.94m, 1.72m, 1m / 332.74m };  // {EUR, NAmerica, SAmerica}
+
+            for (int i = 0; i < 3; i++)
+            {
+                albums[i] = int.Parse(Console.ReadLine());
+                albumPrice[i] = decimal.Parse(Console.ReadLine());
+            }
+            int concerts = int.Parse(Console.ReadLine());
+            decimal profitConcertEUR = decimal.Parse(Console.ReadLine());
+
+            decimal profitAlbums = 0;
+            for (int i = 0; i < 3; i++)
+                profitAlbums += albums[i] * albumPrice[i] * fx[i];
+            profitAlbums *= (1m - 0.35m) * (1m - 0.20m);   // net profit after producer's commission & taxes
+
+            decimal profitConcerts = concerts * profitConcertEUR * fx[0];
+            if (profitConcerts > 100000m)
+                profitConcerts *= (1m - 0.15m);            // net profit after producer's commission
+
+            bool albumsMoreProfitable = profitAlbums > profitConcerts;
+
+            if (albumsMoreProfitable)
+                Console.WriteLine("Let's record some songs! They'll bring us {0:0.00}lv.", profitAlbums);
+            else
+                Console.WriteLine("On the road again! We'll see the world and earn {0:0.00}lv.", profitConcerts);
+        }
+    }
+}
---
+++ 33 Programming Basics Archive/Programming Basics Exam 2015-10-18/02 Grand Theft Examo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Grand_Theft_Examo
+{
+    class _02_Grand_Theft_Examo
+    {
+        static void Main(string[] args)
+        {
+            int escapeAttempts = int.Parse(Console.ReadLine());
+            double beers = 0;
+            double thieves = 0;
+            double thievesSlapped = 0;
+            double thievesEscaped = 0;
+
+            for (int i = 0; i < escapeAttempts; i++)
+            {
+                thieves = double.Parse(Console.ReadLine());
+                beers += double.Parse(Console.ReadLine());
+
+                thievesSlapped += Math.Min(5, thieves);             //slaps max 5 thieves per escape attempt
+                thievesEscaped += thieves - Math.Min(5, thieves);
+            }
+
+            double beers6packs = Math.Truncate(beers / 6);
+            double beersBottles = beers % 6;
+
+            Console.WriteLine("{0} thieves slapped.", thievesSlapped);
+            Console.WriteLine("{0} thieves escaped.", thievesEscaped);
+            Console.WriteLine("{0} packs, {1} bottles.", beers6packs, beersBottles);
+        }
+    }
+}
