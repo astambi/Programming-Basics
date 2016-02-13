e621a1717625b136229734ef85938e03323f4a50
Anna <annsta@gmail.com>
2016-02-14 01:31:09 +0200

    Simple Loops

---
+++ 05 Simple Loops/Equal Pairs 2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_Pairs_2
+{
+    class Equal_Pairs_2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // 2*n  numbers
+            double currentValuePair = double.Parse(Console.ReadLine()) + double.Parse(Console.ReadLine()); //1st sum
+            double previousValuePair = currentValuePair;
+            double currentDiffPairs = 0;
+            double maxDiffPairs = 0;
+            
+            for (int i = 0; i < n-1; i++) //n-1 sums
+            {
+                currentValuePair = double.Parse(Console.ReadLine()) + double.Parse(Console.ReadLine()); 
+                currentDiffPairs = Math.Abs(currentValuePair - previousValuePair);
+                if (currentDiffPairs > maxDiffPairs)
+                    maxDiffPairs = currentDiffPairs;
+                previousValuePair = currentValuePair;
+            }
+            bool noDiffs = maxDiffPairs == 0;
+            if (noDiffs)
+                Console.WriteLine("Yes, value={0}", currentValuePair);
+            else
+                Console.WriteLine("No, maxdiff={0}", maxDiffPairs);
+        }
+    }
+}
---
+++ 05 Simple Loops/Odd Even Position 1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even_Position
+{
+    class Odd_Even_Position
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());           
+            double number = 0.0;
+            double sumEven = 0.0;
+            double minEven = double.MaxValue; 
+            double maxEven = double.MinValue;
+            double sumOdd = 0.0;
+            double minOdd = double.MaxValue;
+            double maxOdd = double.MinValue;
+
+            for (int i = 1; i <= n; i++) // [1,n] numbers
+            {
+                number = double.Parse(Console.ReadLine());
+                bool evenPosition = i % 2 == 0;
+                if (evenPosition)
+                {
+                    sumEven += number;
+                    if (number > maxEven)
+                        maxEven = number;
+                    if (number < minEven)
+                        minEven = number;
+                }
+                else //odd position
+                {
+                    sumOdd += number;
+                    if (number > maxOdd)
+                        maxOdd = number;
+                    if (number < minOdd)
+                        minOdd = number;
+                }
+            }
+            if (n >= 1) 
+                Console.WriteLine("OddSum={0},\nOddMin={1},\nOddMax={2},", sumOdd, minOdd, maxOdd);
+            else 
+                Console.WriteLine("OddSum={0},\nOddMin=No,\nOddMax=No,", sumOdd);
+            if (n >= 2) 
+                Console.WriteLine("EvenSum={0},\nEvenMin={1},\nEvenMax={2}", sumEven, minEven, maxEven);
+            else 
+                Console.WriteLine("EvenSum={0},\nEvenMin=No,\nEvenMax=No", sumEven);                        
+        }
+    }
+}
