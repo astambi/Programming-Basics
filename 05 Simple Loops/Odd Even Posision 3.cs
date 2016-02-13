dbd914cc99a090d5a612cb1128669e5e085eb10c
Anna <annsta@gmail.com>
2016-02-13 17:52:54 +0200

    Simple Loops

--- 05 Simple Loops/Odd Even Posision 3.cs
+++ 05 Simple Loops/Odd Even Posision 3.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Odd_Even_Position_2
+namespace Odd_Even_Position_3
 {
-    class Odd_Even_Position_2
+    class Odd_Even_Position_3
     {
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
             double number = 0.0;
-            double[] min = new double[] { 1000000000.0, 1000000000.0 };     // {odd, even}
-            double[] max = new double[] { -1000000000.0, -1000000000.0 };   // {odd, even}
-            double[] sum = new double[] { 0.0, 0.0 };                       // {odd, even}
-
+            double[] min = new double[] { double.MaxValue, double.MaxValue };   // {odd, even}
+            double[] max = new double[] { double.MinValue, double.MinValue };   // {odd, even}
+            double[] sum = new double[] { 0.0, 0.0 };                           // {odd, even}
+            
             for (int i = 1; i <= n; i++) // [1,n] numbers
             {
                 number = double.Parse(Console.ReadLine());
@@ -29,13 +29,13 @@ namespace Odd_Even_Position_2
                 if (number < min[j])
                     min[j] = number;
             }
-            if (max[0] != -1000000000.0) //neutral value change
+            if (n >= 1) 
                 Console.WriteLine("OddSum={0},\nOddMin={1},\nOddMax={2},", sum[0], min[0], max[0]);
-            else // no change in neutral value => no input 
+            else 
                 Console.WriteLine("OddSum={0},\nOddMin=No,\nOddMax=No,", sum[0]);
-            if (max[1] != -1000000000.0) //neutral value change
+            if (n >= 2) 
                 Console.WriteLine("EvenSum={0},\nEvenMin={1},\nEvenMax={2}", sum[1], min[1], max[1]);
-            else // no change in neutral value => no input
+            else 
                 Console.WriteLine("EvenSum={0},\nEvenMin=No,\nEvenMax=No", sum[1]);
         }
     }
