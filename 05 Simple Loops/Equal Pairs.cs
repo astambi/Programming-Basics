5d9cad9f9bfac17c96f9cc2b80efb73b29fb10d2
Anna <annsta@gmail.com>
2016-02-13 14:52:59 +0200

    Simple Loops

---
+++ 05 Simple Loops/Equal Pairs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_Pairs
+{
+    class Equal_Pairs
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // 2*n  numbers
+            double[] valuePairs = new double[n];
+            double[] diffPairs = new double[n - 1];
+
+            for (int i = 0; i < n; i++)
+                valuePairs[i] = double.Parse(Console.ReadLine()) + double.Parse(Console.ReadLine()); //n sums
+            for (int i = 0; i < n-1; i++)
+                diffPairs[i] = Math.Abs(valuePairs[i + 1] - valuePairs[i]);
+
+            double maxDiffPairs = 0;
+            for (int i = 0; i < n-1; i++) //comparison n-1 diffs
+            {
+                if (diffPairs[i] > maxDiffPairs)
+                    maxDiffPairs = diffPairs[i];                      
+            }
+
+            bool noDiffs = maxDiffPairs == 0;
+            if (noDiffs)
+                Console.WriteLine("Yes, value={0}",valuePairs[0]);
+            else
+                Console.WriteLine("No, maxdiff={0}", maxDiffPairs);
+        }
+    }
+}
