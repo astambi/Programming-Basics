129188cd75a0e03a9f36de28b39ff9d871b4cdc5
Anna <annsta@gmail.com>
2016-02-18 01:20:21 +0200

    Drawing Figures at the Console

---
+++ 06 Drawing Figures at the Console/Christmas Tree 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas_Tree_2
+{
+    class Christmas_Tree_2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                string spaces = new string(' ', n - i);
+                string stars = new string('*', i);
+                Console.WriteLine("{0}{1} | {1}", spaces, stars);
+            }
+        }
+    }
+}
