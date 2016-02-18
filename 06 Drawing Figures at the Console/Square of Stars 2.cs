dc21b77a8c0116a797ce891fff6305d4d01a6f70
Anna <annsta@gmail.com>
2016-02-18 18:56:59 +0200

    Drawing Figures at the Console

---
+++ 06 Drawing Figures at the Console/Square of Stars 2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_of_Stars_2
+{
+    class Square_of_Stars_2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string line = "";
+            for (int i = 0; i < n; ++i)
+                line += "* ";
+            for (int i = 0; i < n; ++i)
+                Console.WriteLine(line);
+        }
+    }
+}
