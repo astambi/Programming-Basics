671e676fba3da9c94d10c476b54bfdfbaba07807
Anna <annsta@gmail.com>
2016-02-21 15:33:58 +0200

    Exams

---
+++ 33 Programming Basics Archive/Programming Basics Exam 2015-04-26 Morning/03 King of Thieves.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_King_of_Thieves
+{
+    class _03_King_of_Thieves
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //n always odd
+            char gemSymbol = Console.ReadLine()[0];
+            char backgroundSymbol = '-';
+            int halfWidth = (n + 1) / 2;
+
+            //upper half
+            int gemCount = 1;          
+            for (int i = 1; i <= halfWidth; i++)
+            {
+                int backgroundCount = (n - gemCount) / 2;
+                string gem = new string(gemSymbol, gemCount);
+                string background = new string(backgroundSymbol, backgroundCount);
+                Console.WriteLine("{0}{1}{0}", background, gem);
+                gemCount += 2;
+            }
+            //lower half
+            gemCount = n - 2;
+            for (int i = halfWidth - 1; i > 0; i--)
+            {
+                int backgroundCount = (n - gemCount) / 2;
+                string gem = new string(gemSymbol, gemCount);
+                string background = new string(backgroundSymbol, backgroundCount);
+                Console.WriteLine("{0}{1}{0}", background, gem);
+                gemCount -= 2;
+            }
+        }
+    }
+}
---
+++ 33 Programming Basics Archive/Programming Basics Exam 2015-08-16/03 Christmas Tree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Christmas_Tree
+{
+    class _03_Christmas_Tree
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int width = 2 * n + 1;
+            int heightTrunk = (n + 1) / 2;
+            char treeSymbol = '^';
+            char snowSymbol = '\'';
+            char groundSymbol = '-';
+            string tree = "";
+            string snow = "";
+
+            // tree top 
+            snow = new string(snowSymbol, n);
+            Console.WriteLine("{0}{1}{0}", snow, treeSymbol);
+            // tree branches
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 3; i <= n; i += 2)
+                {
+                    snow = new string(snowSymbol, (width - i) / 2);
+                    tree = new string(treeSymbol, i);
+                    Console.WriteLine("{0}{1}{0}", snow, tree);
+                }
+            }
+            // lowest branch
+            snow = new string(snowSymbol, (width - n - 2) / 2);
+            tree = new string(treeSymbol, n + 2);
+            Console.WriteLine("{0}{1}{0}", snow, tree);           
+            // trunk
+            snow = new string(snowSymbol, (width - 3) / 2);
+            for (int i = 0; i < heightTrunk; i++)
+                Console.WriteLine("{0}| |{0}", snow);
+            // ground
+            string ground = new string(groundSymbol, width);
+            Console.WriteLine(ground);
+        }
+    }
+}
