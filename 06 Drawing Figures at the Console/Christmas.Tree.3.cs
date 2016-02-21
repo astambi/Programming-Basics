a4ae55dc2117c373bdbb16a59248a3c8ecafb359
Anna Stambolieva <astambi@users.noreply.github.com>
2016-02-21 11:10:04 +0200

    Drawing Figures at the Console

---
+++ 06 Drawing Figures at the Console/Christmas.Tree.3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas_Tree_3
+{
+    class Christmas_Tree_3
+
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char treeSymbol = '*';
+            char backgroundSymbol = ' ';
+
+            for (int i = 0; i <= n; i++)
+            {
+                string background = new string(backgroundSymbol, n - i);
+                string tree = new string(treeSymbol, i);
+                Console.WriteLine("{0}{1} | {1}", background, tree);
+            }
+        }
+    }
+}
