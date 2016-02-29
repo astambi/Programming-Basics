b25354c21467e8979ca90025f0f884bea69c0d7f
Anna <annsta@gmail.com>
2016-02-29 23:56:17 +0200

    Advanced Loops

---
+++ 07 Advanced Loops/Greatest Common Divisor (CGD).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greatest_Common_Divisor__CGD_
+{
+    class Greatest_Common_Divisor_CGD
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            while (Math.Min(a, b) != 0)
+            {
+                int remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+            int GCD = a;
+            Console.WriteLine("{0}", a);
+        }
+    }
+}
---
+++ 07 Advanced Loops/Numbers N...1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_N._._._1
+{
+    class Numbers_N_1
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+                Console.WriteLine("{0}", i);
+        }
+    }
+}
