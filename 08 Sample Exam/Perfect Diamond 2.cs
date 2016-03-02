3f8e4d149578ddea63c2000bb53735f1ec2d6533
Anna <annsta@gmail.com>
2016-03-03 01:13:50 +0200

    Sample Exam

---
+++ 08 Sample Exam/Perfect Diamond 2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Diamond_2
+{
+    class Perfect_Diamond_2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char backgroundSymbol = ' ';
+            string insideSymbol = "-*";
+            string background = "";
+            string inside = "*";
+
+            for (int i = 1; i <= n; i++)
+            {
+                background = new string(backgroundSymbol, n - i);                
+                Console.WriteLine("{0}{1}{0}", background, inside);
+                inside += insideSymbol;
+            }
+            inside = inside.Substring(0, inside.Length - 2);
+            for (int i = n - 1; i >= 1; i--)
+            {
+                background = new string(backgroundSymbol, n - i);                
+                inside = inside.Substring(0, inside.Length - 2);
+                Console.WriteLine("{0}{1}{0}", background, inside);
+            }
+        }
+    }
+}
