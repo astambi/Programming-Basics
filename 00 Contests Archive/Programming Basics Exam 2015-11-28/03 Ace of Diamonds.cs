13574aea12d59c98cea917794aa9ec9181b8958a
Anna <annsta@gmail.com>
2016-03-03 22:51:25 +0200

    Exams

---
+++ 00 Contests Archive/Programming Basics Exam 2015-11-28/03 Ace of Diamonds.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Ace_of_Diamonds
+{
+    class Ace_of_Diamonds
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //upper border
+            string border = new string('*', n);            
+            Console.WriteLine(border); 
+            //top pattern
+            for (int i = 0; i < (n - 2) / 2 + 1; i++) 
+            {
+                string left = new string('-', (n - 3) / 2 - i) + new string('@', i);
+                string right = new string('@', i) + new string('-', (n - 3) / 2 - i);
+                Console.WriteLine("*{0}@{1}*", left, right);
+            }
+            //bottom pattern
+            for (int i = (n - 2) / 2 - 1; i >= 0; i--) 
+            {
+                string left = new string('-', (n - 3) / 2 - i) + new string('@', i);
+                string right = new string('@', i) + new string('-', (n - 3) / 2 - i);
+                Console.WriteLine("*{0}@{1}*", left, right);
+            }
+            //bottom border
+            Console.WriteLine(border); 
+        }
+    }
+}
