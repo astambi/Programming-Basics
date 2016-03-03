d8a2590c8ea06899f37cf9bf4e771b762d646a06
Anna <annsta@gmail.com>
2016-03-03 18:11:18 +0200

    Sample Exam (Coding 101)

---
+++ 08 Sample Exam (Coding 101)/Triangle Area.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Area
+{
+    class Triangle_Area
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine()); // y2 = y3
+
+            double a = Math.Abs(x2 - x3);
+            double h = Math.Abs(y1 - y2);
+            double areaTriangle = a * h / 2;
+
+            Console.WriteLine(areaTriangle);
+        }
+    }
+}
