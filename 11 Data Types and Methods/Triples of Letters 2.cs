fc414b5e376aa16871ac5038f2e861bba61e7216
Anna <annsta@gmail.com>
2016-03-12 19:27:30 +0200

    Data Types and Methods

---
+++ 11 Data Types and Methods/Triples of Letters 2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triples_of_Letters_2
+{
+    class Triples_of_Letters_2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (char letter1 = 'a'; letter1 < 'a' + n; letter1++)
+                for (char letter2 = 'a'; letter2 < 'a' + n; letter2++)
+                    for (char letter3 = 'a'; letter3 < 'a' + n; letter3++)
+                        Console.WriteLine("{0}{1}{2}", letter1, letter2, letter3);
+        }
+    }
+}
